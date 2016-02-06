1a8294e68870dc8ed72783ec39f72b54c08ec93d
vuolankoinen <matti.palomaki@helsinki.fi>
2016-02-06 13:47:25 +0200

    testidata

--- analyysi.m
+++ analyysi.m
@@ -6,3 +6,10 @@ frekv = accumarray(data,ones(1,length(data)));
   uskottavuudet = vertcat(zeros(1,10)', jakopisteet_NB(data, 10));
 
 plot (linspace(1,length(data),length(uskottavuudet)), uskottavuudet) % K?yr?n piikit ovat lupaavia jakopisteit?.                   
+
+%% Edellisen menetelman testaus tunnetulla mallilla:
+
+testidata = horzcat((round(rand(1,200)*30)+1), round((rand(1,6000)*22)+8), (round(rand(1,length(data) - 6200)*22)+1))';
+testiuskottavuudet = vertcat(zeros(1,10)', jakopisteet_NB(testidata, 10));
+
+plot (linspace(1,length(testidata),length(testiuskottavuudet)), testiuskottavuudet,"r") % K?yr?n piikit ovat lupaavia jakopisteit?.                   
