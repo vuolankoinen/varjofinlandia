d6823cc3e775668f6a79a827a74fe504c5fdbdec
vuolankoinen <matti.palomaki@helsinki.fi>
2016-02-06 12:39:26 +0200

    eka kuvaaja

--- analyysi.m
+++ analyysi.m
@@ -1,29 +1,27 @@
-%%Luetaan esikäsitelty runoteos tiedostosta ja analysoidaan sitä.
-
 data = dlmread('data');
 frekv = accumarray(data,ones(1,length(data)));
 
-%% Naive Bayes
-%% Käytetään ns. sanapussi-mallia tekstin kuvaamiseksi, ts. oletetaan sanojen määrien noudattavan multinomijakaumaa.
+%% Naive Bayes                                                                                                                                      
+%% K?ytet??n ns. sanapussi-mallia tekstin kuvaamiseksi, ts. oletetaan sanojen m??rien noudattavan multinomijakaumaa.                                
 
 sanoja1osa = 10; sanoja2osa = length(data)-sanoja1osa;
 frekv1osa = accumarray(data,[ones(1,10) zeros(1,length(data)-sanoja1osa)]);
 frekv2osa = frekv - frekv1osa;
 uskottavuudet = zeros(length(data), 4);
-for (san = 11:length(data)-11)
+for (san = sanoja1osa+1:length(data)-sanoja1osa-1)
   frekv1osa(data(san)) = frekv1osa(data(san)) + 1;
   frekv2osa(data(san)) = frekv2osa(data(san)) - 1;
-  ln_toden1 = log((frekv1osa + 1)./(san + length(frekv))); % Laplace-korjaus on mukana näissä.
-  ln_toden2 = log((frekv2osa + 1)./(length(data) - san + length(frekv))); % Laplace-korjaus on mukana näissä.
+  ln_toden1 = log((frekv1osa + 1)./(san + length(frekv))); % Laplace-korjaus on mukana n?iss?.                                                      
+  ln_toden2 = log((frekv2osa + 1)./(length(data) - san + length(frekv))); % Laplace-korjaus on mukana n?iss?.                                       
   uskottavuudet(san, 1) = sum(ln_toden1.*frekv1osa);
   uskottavuudet(san, 2) = sum(ln_toden2.*frekv1osa);
   uskottavuudet(san, 3) = sum(ln_toden1.*frekv2osa);
   uskottavuudet(san, 4) = sum(ln_toden2.*frekv2osa);
 end
-uskottavuudet = uskottavuudet(11:end-11,:);
 
-erot1 = uskottavuudet(:,1) ./ uskottavuudet(:,2); % Mitä suurempia nämä ovat,
-erot2 = uskottavuudet(:,4) ./ uskottavuudet(:,3); % sitä uskottavampi jakopiste.
-erot = erot1.*erot2;
-plot (linspace(min(erot),max(erot),length(erot)), erot) % Käyrän piikit ovat lupaavia jakopisteitä.
+uskottavuudet = uskottavuudet(sanoja1osa+1:end-1-sanoja1osa,:);
+
+yhteisuskottavuus = uskottavuudet(:,1) -uskottavuudet(:,2) +uskottavuudet(:,4) -uskottavuudet(:,3);
+
+plot (linspace(sanoja1osa,length(data),length(yhteisuskottavuus)), yhteisuskottavuus) % K?yr?n piikit ovat lupaavia jakopisteit?.                   
 1
