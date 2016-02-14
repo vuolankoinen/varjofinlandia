bf4516553c93855872dcadb25caa0658cc37d5bc
matti <matti.palomaki@helsinki.fi>
2016-02-14 03:07:07 +0200

    testimenetelmää

--- analyysi.m
+++ analyysi.m
@@ -2,7 +2,7 @@ data = dlmread('data');
 frekv = accumarray(data,ones(1,length(data)));
 
 %% Naive Bayes                                                                                                                                      
-%% K?ytet??n ns. sanapussi-mallia tekstin kuvaamiseksi, ts. oletetaan sanojen m??rien noudattavan multinomijakaumaa.                                
-  uskottavuudet = vertcat(zeros(1,10)', jakopisteet_NB(data, 10));
+%% Käytetään ns. sanapussi-mallia tekstin kuvaamiseksi, ts. oletetaan sanojen määrien noudattavan multinomijakaumaa.                                
+uskottavuudet = jakopisteet_NB(data, 10);
 
-plot (linspace(1,length(data),length(uskottavuudet)), uskottavuudet) % K?yr?n piikit ovat lupaavia jakopisteit?.                   
+plot (linspace(1,length(data),length(uskottavuudet)), uskottavuudet) % Käyrän piikit ovat lupaavia jakopisteitä.
--- testit.m
+++ testit.m
@@ -8,7 +8,7 @@ varit = 'rbgrbg'
 hold off
 for (tt = 1:6)
   testidata1 = empirical_rnd(data);
-  testiuskottavuudet1 = vertcat(zeros(1,10)', jakopisteet_NB(testidata1, 10));
+  testiuskottavuudet1 = jakopisteet_NB(testidata1, 10);
   plot (linspace(1,length(testidata1),length(testiuskottavuudet1)), testiuskottavuudet1,varit(tt))
   hold on
 end
@@ -20,7 +20,7 @@ hold off
 hold off
 for (tt = 1:6)
   testidata2 = empirical_rnd(data, rows(data), 1);
-  testiuskottavuudet2 = vertcat(zeros(1,10)', jakopisteet_NB(testidata2, 10));
+  testiuskottavuudet2 = jakopisteet_NB(testidata2, 10);
   plot (linspace(1,length(testidata2),length(testiuskottavuudet2)), testiuskottavuudet2,varit(tt))
   hold on
 end
