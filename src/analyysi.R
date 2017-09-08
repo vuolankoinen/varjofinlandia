196392621bd81f3384dd310d45cb8cbb5cd43567
vuolankoinen <matti.palomaki@helsinki.fi>
2017-09-08 16:21:13 +0300

    uskottavuuksien laskemisen hiomista

--- src/analyysi.R
+++ src/analyysi.R
@@ -1,8 +1,6 @@
 teksti <- read.csv("data")
 teksti <- as.vector(teksti$X1)
 
-source(file="frekvenssit.R")
-
 frekv <- table(teksti)
 frekv <- cbind(2:(nrow(frekv)+1), frekv)
 
@@ -12,14 +10,13 @@ teksti[teksti %in% yksit] <- 0
 
 frekv <- cbind(c(0,as.vector(frekv[frekv[,2]>1,1])), table(teksti))
 
-source("jaon_uskottavuus.R")
 source("jakopisteet.R")
 
 # jaot500 <- jakopisteet(teksti, 500) # Puolisen minuuttia
 
-#jaot50 <- jakopisteet(teksti, 50) # 4½ minuuttia
+jaot50 <- jakopisteet(teksti, 50) # 4½ minuuttia
 #save(jaot50, file="jaot50.Rdat")
-load("jaot50.Rdat")
+#load("jaot50.Rdat")
 
 png("jaot50.png")
 plot(seq(50,length(teksti)-1,by=50), jaot50)
@@ -32,12 +29,9 @@ png("jakojen_erot50.png")
 plot(seq(50,length(teksti)-1,by=50), erot50)
 dev.off()
 
-source("jaon_normalisoitu_uskottavuus.R")
-
-#jaot50mod <- jakopisteet(teksti, 50, 1) # 5 min
-
+jaot50mod <- jakopisteet(teksti, 50, 1) # 5 min
 #  save(jaot50mod, file="jaot50mod.Rdat")
-load("jaot50mod.Rdat")
+#load("jaot50mod.Rdat")
 
 png("jaot50normalisoitu.png")
 plot(seq(50,length(teksti)-1,by=50), jaot50mod)
--- src/jakopisteet.R
+++ src/jakopisteet.R
@@ -1,17 +1,22 @@
 # Laskee sanalistan mahdollisten kahtiajakopisteiden uskottavuudet. Parametreina sanalista, sanojen frekvenssit ja tutkittavien jakopisteiden taajuus.
 # modattu = 0 -> uskottavuus, modattu = 1 -> normalisoitu uskottavuus
+# rajat antaa aloitus- ja lopetusindeksit: rajat=c(alku, loppu)
 
 source("jaon_uskottavuus.R")
 source("jaon_normalisoitu_uskottavuus.R")
 
-jakopisteet <- function(sanalista, osakoko=1, modattu=0) {
+jakopisteet <- function(sanalista, osakoko=1, modattu=0, rajat=c(0)) {
     tulos <- c()
+    if (length(rajat)!=2) {
+        rajat <- c(osakoko, length(sanalista))
+    }
+    indeksit <- seq(rajat[1],rajat[2]-1,by=osakoko)
     if (modattu==0){
-        for (tt in seq(osakoko,length(sanalista)-1,by=osakoko)) {
+        for (tt in indeksit) {
             tulos <-    c(tulos, jaon_uskottavuus(sanalista, tt))
         }
     } else {
-        for (tt in seq(osakoko,length(sanalista)-1,by=osakoko)) {
+        for (tt in indeksit) {
             tulos <-    c(tulos, jaon_normalisoitu_uskottavuus(sanalista, tt))
             cat(tt, "\n")
         }
