449f9880e17534233d70c258dd9f820c09d75116
Matti Palomäki <vuolankoinen@users.noreply.github.com>
2018-01-28 17:17:27 +0200

    Jakopisteet pois silmukoilta

--- src/jakopisteet.R
+++ src/jakopisteet.R
@@ -6,20 +6,18 @@ source("jaon_uskottavuus.R")
 source("jaon_normalisoitu_uskottavuus.R")
 
 jakopisteet <- function(sanalista, osakoko=1, modattu=0, rajat=c(0)) {
-    tulos <- c()
-    if (length(rajat)!=2) {
-        rajat <- c(osakoko, length(sanalista))
-    }
-    indeksit <- seq(rajat[1],rajat[2]-1,by=osakoko)
-    if (modattu==0){
-        for (tt in indeksit) {
-            tulos <-    c(tulos, jaon_uskottavuus(sanalista, tt))
-        }
-    } else {
-        for (tt in indeksit) {
-            tulos <-    c(tulos, jaon_normalisoitu_uskottavuus(sanalista, tt))
-            cat(tt, "\n")
-        }
-    }
-    return(tulos)
+  if (length(rajat)!=2) {
+    rajat <- c(osakoko, length(sanalista))
+  }
+  indeksit <- seq(rajat[1],rajat[2]-1,by=osakoko)
+  if (modattu == 0) {
+    tulos <- vapply(indeksit, 
+                    function (tt) jaon_uskottavuus(sanalista, tt),
+                    numeric(1))
+  } else {
+    tulos <- vapply(indeksit, 
+                    function (tt) jaon_normalisoitu_uskottavuus(sanalista, tt),
+                    numeric(1))  
+  }
+  return(tulos)
 }
