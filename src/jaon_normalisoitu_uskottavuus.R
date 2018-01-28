2c779cee69cef6a4979da220a6903b9e337eae5b
Matti Palomäki <vuolankoinen@users.noreply.github.com>
2018-01-28 17:01:50 +0200

    typo

--- src/jaon_normalisoitu_uskottavuus.R
+++ src/jaon_normalisoitu_uskottavuus.R
@@ -8,18 +8,17 @@ jaon_normalisoitu_uskottavuus <- function (sanat, jakopiste) {
   fr2 <- frekvenssit(sanat[(jakopiste+1):(length(sanat))])
   N2 <- length(sanat) - jakopiste
   
-  ln_usk1 <- sum(vapply(
-    sanat[1:jakopiste],
-    function (tt) log(as.numeric(fr1[fr1[,1]==tt,2])),
-    numeric(1))
+  ln_usk1 <- sum(vapply(sanat[1:jakopiste],
+                        function (tt) log(as.numeric(fr1[fr1[,1]==tt,2])),
+                        numeric(1)))
   summa <- sum(vapply(sanat[1:jakopiste],
                       function (tt) as.numeric(fr1[fr1[,1]==tt,2]),
                       numeric(1)))
-  ## nimittäjät: - N1 * log(N1) 
-  ## Normalisointi: jaetaan uskottavuuden jokainen empiirinen todennäköisyys keskimääräisellä empiirisellä todennäköisyydellä, eli: N1*( 2*log(N1) - log(summa) )
-  ## Yhdessä siis
+    ## nimittäjät: - N1 * log(N1)
+    ## Normalisointi: jaetaan uskottavuuden jokainen empiirinen todennäköisyys keskimääräisellä empiirisellä todennäköisyydellä, eli: N1*( 2*log(N1) - log(summa) )
+    ## Yhdessä siis
   ln_usk1 <- ln_usk1 +N1*log(N1) - N1*log(summa)
-  
+    
   ln_usk2 <- sum(vapply(sanat[(jakopiste+1):(length(sanat))],
                         function (tt) log(as.numeric(fr2[fr2[,1]==tt,2])), # Uskottavuus on tulo kunkin havaitun sanan empriisestä todennäköisyydestä.
                         numeric(1)))
