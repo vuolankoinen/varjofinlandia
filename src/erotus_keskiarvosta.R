ec9b829bc3814dc3dabb79e733f4711a3730b991
Matti Palomäki <vuolankoinen@users.noreply.github.com>
2018-01-28 13:23:07 +0200

    Erotus keskiarvosta silmukoilta vektoroiduksi
    lapply -> sapply

--- src/erotus_keskiarvosta.R
+++ src/erotus_keskiarvosta.R
@@ -1,5 +1,5 @@
 # Palauttaa kunkin alkion eron ympäröivien alkioiden keskiarvoon.
 
 erotus_keskiarvosta <- function (jono, kantama=2) {
-    as.numeric(lapply(1:length(jono), function (tt) jono[tt] - mean(jono[(max(1,tt-kantama)):(min(tt+kantama,length(jono)))])))
+    sapply(1:length(jono), function (tt) jono[tt] - mean(jono[(max(1,tt-kantama)):(min(tt+kantama,length(jono)))]))
 }
